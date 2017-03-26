bb3f65633a7d925c16715c15c6aab2467260982a
AlexanderBrenchev <alexzund3r@yahoo.com>
2017-03-26 15:24:29 +0700

    Strict mode and other fixes

--- js/stat.js
+++ js/stat.js
@@ -1,39 +1,41 @@
+'use strict';
 
 function renderStatistics(ctx, names, times) {
-  var playersData = [];
+  var cloudX = 110,
+      cloudY = 20,
+      cloudWidth = 420,
+      cloudHeight = 270;
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillRect(cloudX, cloudY, cloudWidth, cloudHeight);
 
   ctx.fillStyle = '#ffffff';
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillRect(cloudX - 10, cloudY - 10, cloudWidth, cloudHeight);
 
   ctx.font = '16px "PT Mono"';
   ctx.fillStyle = '#000000';
   ctx.fillText('Ура, вы победили!', 230, 40);
   ctx.fillText('Список результатов:', 225, 70);
 
-  names.forEach(function (name, idx) {
-    playersData.push({
-      name: name,
-      score: Math.round(times[idx])
-    });
-  });
-
   var currentX = 130,
-      currentY = 260;
-  playersData.forEach(function(elem) {
+      currentY = 260,
+      barWidth = 40,
+      barMarginRight = 50,
+      nameMarginTop = 20,
+      scoreMarginBottom = 30;
+  names.forEach(function(playerName, idx) {
     ctx.fillStyle = '#000000';
-    ctx.fillText(elem.name, currentX, currentY);
+    ctx.fillText(playerName, currentX, currentY);
     var randomOpacity = Math.random();
     ctx.fillStyle = 'rgba(0, 0, 255, ' + randomOpacity + ')';
-    if (elem.name == 'Вы') {
+    if (playerName == 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    var barHeight = elem.score / 100;
-    ctx.fillRect(currentX, currentY - barHeight - 20, 40, barHeight);
+    var playerTime = Math.round(times[idx]);
+    var barHeight = playerTime / 100;
+    ctx.fillRect(currentX, currentY - barHeight - nameMarginTop, barWidth, barHeight);
     ctx.fillStyle = '#000000';
-    ctx.fillText(elem.score, currentX, currentY - barHeight - 30);
-    currentX += 90;
+    ctx.fillText(playerTime, currentX, currentY - barHeight - scoreMarginBottom);
+    currentX += barWidth + barMarginRight;
   });
 }
